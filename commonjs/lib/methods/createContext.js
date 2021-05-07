3e108a4e56c99f6365ce38db7c0b1e766be5f01b
TofuDefi <72276741+tofudefi@users.noreply.github.com>
2021-05-07 13:24:47 +0800

    switch to trongrid

--- commonjs/lib/methods/createContext.js
+++ commonjs/lib/methods/createContext.js
@@ -8,8 +8,8 @@ const axios_1 = __importDefault(require("axios"));
 function networkToTronApiUrl(network = "mainnet") {
     switch (network) {
         case "mainnet":
-//            return "https://api.trongrid.io";
-            return "https://java-tron.opentron.org";
+            return "https://api.trongrid.io";
+//            return "https://java-tron.opentron.org";
         case "shasta":
             return "https://api.shasta.trongrid.io";
         case "nile":
